3a837ad032d99463b732ea65b9b7e4fe2a548167
codytheking <codytheking93@gmail.com>
2021-08-31 11:18:49 -0700

    Updating variables example

---
+++ InClass/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int list = 10;
+        System.out.println("List: " + list);
+        list = 7;
+        System.out.println("List: " + list);
+        list = list - 2;
+        System.out.println("List: " + list);
+    }
+}
